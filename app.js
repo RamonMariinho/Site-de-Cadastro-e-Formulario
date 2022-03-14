e3ed0b7f81e7fb4e84dc6f0c26f0f14a99d17b20
RamonMariinho <101571794+RamonMariinho@users.noreply.github.com>
2022-03-14 10:32:51 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme')
+    var className = document.body.className;
+    
+    if(className== "light-teme"){
+    this.textContet="Dark";
+    }
+    else{
+        this.textContet = "Light";
+    }
+    console.log ('current class name:'+ className);
+});
